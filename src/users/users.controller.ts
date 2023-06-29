a1cb4d0c3267073077cad1d36be18f293d49bdc6
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-06-28 22:51:49 -0400

    feat: ✨ finished module users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,38 +5,45 @@ import {
 	Body,
 	Patch,
 	Param,
-	Delete
+	Delete,
+	HttpCode,
+	HttpStatus
 } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { User } from './entities/user.entity'
 
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	@Post()
-	create(@Body() createUserDto: CreateUserDto) {
-		return this.usersService.create(createUserDto)
-	}
-
-	@Get()
-	findAll() {
-		return this.usersService.findAll()
-	}
-
-	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.usersService.findOne(+id)
+	async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+		return await this.usersService.create(createUserDto)
 	}
 
 	@Patch(':id')
-	update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-		return this.usersService.update(+id, updateUserDto)
+	async update(
+		@Param('id') id: string,
+		@Body() updateUserDto: UpdateUserDto
+	): Promise<void> {
+		return await this.usersService.update(+id, updateUserDto)
+	}
+
+	@Get()
+	async findAll(): Promise<User[]> {
+		return await this.usersService.findAll()
+	}
+
+	@Get(':id')
+	async findOne(@Param('id') id: number): Promise<User> {
+		return await this.usersService.findOne(id)
 	}
 
 	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return this.usersService.remove(+id)
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async remove(@Param('id') id: number): Promise<void> {
+		return await this.usersService.remove(id)
 	}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class UsersService {
-	create(createUserDto: CreateUserDto) {
-		return 'This action adds a new user'
+	constructor(
+		@InjectRepository(User) private userRepository: Repository<User>
+	) {}
+	async create(createUserDto: CreateUserDto): Promise<User> {
+		const user = this.userRepository.create(createUserDto)
+		return await this.userRepository.save(user)
 	}
 
-	findAll() {
-		return `This action returns all users`
+	async update(userId: any, updateUserDto: UpdateUserDto): Promise<void> {
+		const userDTO = await this.findOne(userId)
+		const user = this.userRepository.create(userDTO)
+		this.userRepository.merge(user, updateUserDto)
+		await this.userRepository.save(user)
 	}
 
-	findOne(id: number) {
-		return `This action returns a #${id} user`
+	async findAll(): Promise<User[]> {
+		return await this.userRepository.find({
+			select: ['id', 'name', 'email', 'role', 'status'],
+			where: { status: true }
+		})
 	}
 
-	update(id: number, updateUserDto: UpdateUserDto) {
-		return `This action updates a #${id} user`
+	async findOne(id: number): Promise<User> {
+		const user = await this.userRepository.findOneOrFail({
+			select: ['id', 'name', 'email', 'role', 'status'],
+			where: { id }
+		})
+
+		if (!id) {
+			throw new NotFoundException(`Não achei um usuario com o id ${id}`)
+		}
+		return user
 	}
 
-	remove(id: number) {
-		return `This action removes a #${id} user`
+	async findEmail(email: string): Promise<User> {
+		const user = await this.userRepository.findOneOrFail({
+			select: ['id', 'name', 'email', 'role', 'status', 'password'],
+			where: { email }
+		})
+
+		if (!user) {
+			throw new NotFoundException(`Não achei um usuario com o Email ${user}`)
+		}
+		return user
+	}
+
+	async remove(id: number): Promise<void> {
+		await this.findOne(id)
+
+		if (!id) {
+			throw new NotFoundException(`Não achei um Usuario com o id ${id}`)
+		}
+		this.userRepository.softDelete({ id })
 	}
 }
