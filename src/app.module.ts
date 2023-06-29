cb51feafb640ef29f48b7cf3d83cfee53a9e8d15
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-06-28 22:50:23 -0400

    chore(deps): 🔗 add dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
+import { HttpModule } from '@nestjs/axios'
+import { User } from './users/entities/user.entity'
 
 @Module({
-	imports: [UsersModule],
+	imports: [
+		UsersModule,
+		HttpModule,
+		TypeOrmModule.forRoot({
+			type: 'sqlite',
+			database: 'db',
+			entities: [User],
+			synchronize: true
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
